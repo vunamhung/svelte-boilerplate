c75e88156abbdefb8db4614c7ad6ee1aa2eb4a8f
Hung Vu <vunamhung@gmail.com>
2021-04-14 10:40:13 +0700

    support electron

---
+++ preload.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ipcRenderer } from 'electron'
+
+// Since we disabled nodeIntegration we can reintroduce
+// needed node functionality here
+process.once('loaded', () => {
+  global.ipcRenderer = ipcRenderer
+})
